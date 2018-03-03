02c020b56c74db2280c600d0189dea5f78425e49
eder <eder@fiap.com.br>
2018-03-03 13:02:36 -0300

    Gerador de Relatórios

---
+++ src/main/java/br/com/fiap/roupas/util/GeradorRelatorio.java
@@ -0,0 +1,76 @@
+package br.com.fiap.roupas.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import br.com.fiap.roupas.roupasfiap.model.Item;
+import br.com.fiap.roupas.roupasfiap.model.Pedido;
+
+
+public class GeradorRelatorio {
+
+	private int numeroitem = 0;
+	private int qtdpedido = 0;
+	
+	public void gerarCupom(Pedido pedido) {
+		Document document =  new  Document();
+		
+		try {
+			PdfWriter write = PdfWriter.getInstance(document, new FileOutputStream("C:\\relatorios\\relatorioPedido"));
+			document.open();
+			
+			document.add(new Paragraph("                          " + pedido.getEmpresa().getNome() + "                     "));
+			document.add(new Paragraph("                          " + pedido.getEmpresa().getEndereco() + "                 "));
+			document.add(new Paragraph("                              SÃO PAULO / SP                    "));
+			document.add(new Paragraph("                                                                                    "));
+	
+			document.add(new Paragraph("CNPJ: " + pedido.getEmpresa().getCpnj()));			
+			document.add(new Paragraph("IE: " + pedido.getEmpresa().getIncricaoEstadual()));
+			document.add(new Paragraph("IM: " + pedido.getEmpresa().getInscricaoMunicipal()));
+			document.add(new Paragraph("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"));
+			document.add(new Paragraph(pedido.getDataPedido() + "CCF: 010333" +                     "COO: 123456"));
+			document.add(new Paragraph("                                                                                    "));
+			document.add(new Paragraph("CUPOM FISCAL"));
+			document.add(new Paragraph("                                                                                    "));
+			document.add(new Paragraph("ITEM   CODIGO   DESCRIÇÃO    QTD.  UN.    VL UNIT(R$)  ST  VL  ITEM(R$)"));
+			document.add(new Paragraph("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"));
+			
+			
+			pedido.getProdutoList().entrySet().forEach(item -> {
+			    //System.out.println("Key : " + entry.getKey(). + " Value : " + entry.getValue());
+			    try {
+					document.add(new Paragraph(numeroitem + "  " + item.getKey().getId() + "  " +  item.getKey().getDescricao() +
+													"  " + item.getValue() + "   " + item.getKey().getValor()));
+				} catch (DocumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}); 
+			
+			pedido.getProdutoList().forEach((key, value) -> {
+				numeroitem++;
+				
+			});
+			
+			
+			
+			
+		} catch (FileNotFoundException | DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
