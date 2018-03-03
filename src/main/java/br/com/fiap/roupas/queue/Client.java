3bd20b58bbde846542f5bf5ae475afb5c6bf69a8
eder <eder@fiap.com.br>
2018-03-03 16:06:16 -0300

    Criação das Filas

---
+++ src/main/java/br/com/fiap/roupas/queue/Client.java
@@ -0,0 +1,16 @@
+package br.com.fiap.roupas.queue;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/br/com/fiap/roupas/queue/Produce.java
@@ -0,0 +1,17 @@
+package br.com.fiap.roupas.queue;
+
+import java.io.Serializable;
+
+public class Produce implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
